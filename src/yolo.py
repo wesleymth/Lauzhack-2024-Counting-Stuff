1f950dccd1c2fa30d8085a4ddaac406089aaa670
CamilleChallier <camille.challier@epfl.ch>
2024-11-30 22:26:16 +0100

    update

--- src/yolo.py
+++ src/yolo.py
@@ -11,16 +11,18 @@ def predict(chosen_model, img, classes=[], conf=0.5):
 
     return results
 
-def predict_and_detect(chosen_model, img, classes=[], conf=0.5, rectangle_thickness=2, text_thickness=1):
-    results = predict(chosen_model, img, classes, conf=conf)
+def predict_and_detect(chosen_model, imgs, classes=[], conf=0.5, rectangle_thickness=2, text_thickness=1):
+    
+    results = predict(chosen_model, imgs, classes, conf=conf)
     name = results[0].names[int(classes[0])] if classes else "all" 
     
     all_data = []
-    for result in results:
+    for result, img in zip(results, imgs):
         res = {
         "boxes": [],
         "cls" :[]
             }
+        res["original_image"] = img.copy()
         for box in result.boxes:
             cv2.rectangle(img, (int(box.xyxy[0][0]), int(box.xyxy[0][1])),
                           (int(box.xyxy[0][2]), int(box.xyxy[0][3])), (255, 0, 0), rectangle_thickness)
@@ -29,18 +31,20 @@ def predict_and_detect(chosen_model, img, classes=[], conf=0.5, rectangle_thickn
                         cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), text_thickness)
             res["boxes"].append(box.xyxy[0])
             res["cls"].append(box.cls[0])
+        res["image_with_boxes"] = img
         all_data.append(res)
-    return img, all_data, name
+    return all_data, name
 
-def predict_and_detect_obb(chosen_model, img, classes=[], conf=0.5, rectangle_thickness=2, text_thickness=1):
-    results = predict(chosen_model, img, classes, conf=conf)
+def predict_and_detect_obb(chosen_model, imgs, classes=[], conf=0.5, rectangle_thickness=2, text_thickness=1):
+    results = predict(chosen_model, imgs, classes, conf=conf)
     name = results[0].names[int(classes[0])] if classes else "all"
     all_data = []
-    for result in results:
+    for result, img in zip(results, imgs):
         res = {
         "boxes": [],
         "cls" :[]
             }
+        res["original_image"] = img.copy()
         for box, cls in zip(result.obb.xyxy, result.obb.cls):
             cv2.rectangle(img, (int(box[0]), int(box[1])),
                           (int(box[2]), int(box[3])), (255, 0, 0), rectangle_thickness)
@@ -49,6 +53,7 @@ def predict_and_detect_obb(chosen_model, img, classes=[], conf=0.5, rectangle_th
                         cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), text_thickness)
             res["boxes"].append(box)
             res["cls"].append(cls)
+        res["image_with_boxes"] = img
         all_data.append(res)
     
     return img, all_data, name
@@ -77,10 +82,10 @@ def is_in_construction(results, threshold = 100):
         image["is_in_construction"] = is_in_construction
     return results
 
-def count_people(image, conf=0.5):
-    image = cv2.imread(image)
+def count_people(images, conf=0.5):
+    images = [cv2.imread(image) for image in images]
     model = YOLO("yolo11x.pt")
-    result_img, res, name = predict_and_detect(model, image, classes=[0], conf=conf) 
+    res, name = predict_and_detect(model, images, classes=[0], conf=conf) 
     with open(f'data/results_{name}.pkl', 'wb') as f:
         pickle.dump(res, f)
     return res
@@ -89,7 +94,7 @@ def count_people_tool(image_path : str) -> int:
     """
     Count the number of people in an image
     """
-    res = count_people(image_path)
+    res = count_people([image_path])
     count  = len(res[0]["boxes"])
     return count
 
